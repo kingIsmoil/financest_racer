4bfa68575d1f42209a86a1434cd5e8bc26b57dad
ismoil <ismoilumarzoda@gmail.com>
2025-05-25 16:46:36 +0500

    show HTML and urls

--- finance/urls.py
+++ finance/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('register', views.register_view, name='reg'),
     path('login', views.login_view, name='log'),
     path('logout', views.logout_view, name='lout'),
+    path('showrasxod', views.show_rasxod, name='showr'),
+    path('showdoxod', views.show_doxod, name='showd'),
 ]
